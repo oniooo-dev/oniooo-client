cb6830df467980a6fcb1eef91d26b7fb83303e2b
Donghao Zeng <do_zeng@live.concordia.ca>
2024-08-06 17:51:14 -0400

    some other stuff added

--- src/features/melody/melodyThunks.ts
+++ src/features/melody/melodyThunks.ts
@@ -23,6 +23,7 @@ export const fetchConversationHistory = createAsyncThunk<Conversation[], void, {
     'melody/fetchConversationHistory',
     async (_, { rejectWithValue }) => {
         try {
+            // No request data
             const response = await axios.get('/api/v1/melody/conversations');
             return response.data;     // should return an array of conversations
         } catch (error: any) {
@@ -40,6 +41,7 @@ export const fetchConversationMessages = createAsyncThunk<ConversationMessage[],
     'melody/fetchConversationMessages',
     async (params: { conversationId: string }, { rejectWithValue }) => {
         try {
+            // No request data
             const response = await axios.get(`/api/v1/melody/conversation/${params.conversationId}/messages`);
             return response.data;     // should return an array of messages
         } catch (error: any) {
@@ -48,6 +50,83 @@ export const fetchConversationMessages = createAsyncThunk<ConversationMessage[],
                 return rejectWithValue({ message: error.response.data.message, name: 'MelodyFetchError' });
             } else {
                 return rejectWithValue({ message: error.message, name: 'MelodyFetchError' });
+            }
+        }
+    }
+)
+
+export const createConversation = createAsyncThunk<void, { firstPrompt: string }, { rejectValue: MelodyFetchError }>(
+    'melody/createConversation',
+    async (params: { firstPrompt: string }, { rejectWithValue }) => {
+        try {
+            const requestData: { firstPrompt: string } = {
+                firstPrompt: params.firstPrompt
+            }
+            const response = await axios.post(`/api/v1/melody/conversation`, requestData);
+            return response.data;     // should return an array of messages
+        } catch (error: any) {
+            if (error.response && error.response.data) {
+                // Assuming the backend sends { message: string } in the response body
+                return rejectWithValue({ message: error.response.data.message, name: 'MelodyFetchError' });
+            } else {
+                return rejectWithValue({ message: error.message, name: 'MelodyFetchError' });
+            }
+        }
+    }
+)
+
+export const createConversationMessage = createAsyncThunk<void, { conversationId: string, prompt: string }, { rejectValue: MelodyPostError }>(
+    'melody/createConversationMessage',
+    async (params: { conversationId: string, prompt: string }, { rejectWithValue }) => {
+        try {
+            const requestData: { prompt: string } = {
+                prompt: params.prompt
+            }
+            const response = await axios.post(`/api/v1/melody/conversation`, requestData);
+            return response.data;     // should return an array of messages
+        } catch (error: any) {
+            if (error.response && error.response.data) {
+                // Assuming the backend sends { message: string } in the response body
+                return rejectWithValue({ message: error.response.data.message, name: 'MelodyPostError' });
+            } else {
+                return rejectWithValue({ message: error.message, name: 'MelodyPostError' });
+            }
+        }
+    }
+)
+
+export const updateConversationTitle = createAsyncThunk<void, { conversationId: string, newTitle: string }, { rejectValue: MelodyPutError }>(
+    'melody/updateConversationTitle',
+    async (params: { conversationId: string, newTitle: string }, { rejectWithValue }) => {
+        try {
+            const requestData: { newTitle: string } = {
+                newTitle: params.newTitle
+            }
+            const response = await axios.post(`/api/v1/melody/conversation/${params.conversationId}/title`, requestData);
+            return response.data;     // should return an array of messages
+        } catch (error: any) {
+            if (error.response && error.response.data) {
+                // Assuming the backend sends { message: string } in the response body
+                return rejectWithValue({ message: error.response.data.message, name: 'MelodyPutError' });
+            } else {
+                return rejectWithValue({ message: error.message, name: 'MelodyPutError' });
+            }
+        }
+    }
+)
+
+export const deleteConversationTitle = createAsyncThunk<void, { conversationId: string }, { rejectValue: MelodyPutError }>(
+    'melody/deleteConversation',
+    async (params: { conversationId: string, }, { rejectWithValue }) => {
+        try {
+            const response = await axios.delete(`/api/v1/melody/conversation/${params.conversationId}`);
+            return response.data;     // should return an array of messages
+        } catch (error: any) {
+            if (error.response && error.response.data) {
+                // Assuming the backend sends { message: string } in the response body
+                return rejectWithValue({ message: error.response.data.message, name: 'MelodyPutError' });
+            } else {
+                return rejectWithValue({ message: error.message, name: 'MelodyPutError' });
             }
         }
     }
