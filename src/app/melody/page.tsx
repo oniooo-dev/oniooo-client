5cbbcb44dd17746934f76c9f6718c66d8e0023de
SaintSauce <81129360+SaintSauce@users.noreply.github.com>
2024-08-22 21:52:28 -0400

    removed lines

--- src/app/melody/page.tsx
+++ src/app/melody/page.tsx
@@ -13,14 +13,14 @@ export default function MelodyPage() {
 		<div className="flex flex-row w-full h-full">
 			{isAuthenticated ? (
 				<>
-					<div className="w-[1px] bg-gray-300 bg-opacity-20 z-10"></div> {/* This is a line. */}
+					{/* <div className="w-[1px] bg-gray-300 bg-opacity-20 z-10"></div> This is a line. */}
 					<SideExtensions />
-					<div className="w-[1px] bg-gray-300 bg-opacity-20 z-10"></div> {/* This is a line. */}
+					{/* <div className="w-[1px] bg-gray-300 bg-opacity-20 z-10"></div> This is a line. */}
 					<ChatInterface />
 				</>
 			) : (
 				<>
-					<div className="w-[1px] bg-gray-300 bg-opacity-20 z-10"></div> {/* This is a line. */}
+					{/* <div className="w-[1px] bg-gray-300 bg-opacity-20 z-10"></div> This is a line. */}
 					<UnsignedChatInterface />
 				</>
 			)}
