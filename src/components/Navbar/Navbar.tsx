af82da9d6cdfe05be2f57aa43895b43cf705f274
SaintSauce <81129360+SaintSauce@users.noreply.github.com>
2024-09-08 19:27:08 -0400

    navbar fix

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useCallback, useEffect, useState } from "react";
-import NavbarLink from "./NavbarLink";
+// import NavbarLink from "./NavbarLink";
 import { NAVBAR_LINKS } from "@/lib/constants";
 import { usePathname } from "next/navigation";
 
@@ -16,50 +16,12 @@ const Navbar = () => {
 	}, []);
 
 	useEffect(() => {
-<<<<<<< HEAD
 		setSelectedPage(pathname);
 	}, [pathname]);
 
 	return (
 		<div className="flex flex-row gap-2 w-full">
-=======
-		// Set the initial state once the component is mounted on the client
-		setSelectedPage(window.location.pathname);
-
-		// Add event listener for popstate event on window
-		window.addEventListener("popstate", handleRouteChange);
-
-		// Clean up the event listener on component unmount
-		return () => {
-			window.removeEventListener("popstate", handleRouteChange);
-		};
-	}, []);
-
-	useEffect(() => {
-		console.log("Authentication status changed:", isAuthenticated ? "Authenticated" : "Not authenticated");
-		if (isAuthenticated) {
-			setIsAuthModalOpen(false);
-		}
-	}, [isAuthenticated]);
-
-	if (pathname === "/") {
-		return null;
-	}
-
-	return (
-		<div className="flex flex-row gap-2 px-2 w-full">
-			<div
-				onClick={handleProfileIconClick}
-				className="flex items-center justify-center w-11 h-11 rounded-lg bg-white bg-opacity-0 hover:bg-opacity-20 duration-500 cursor-pointer"
-			>
-				{isAuthenticated ? (
-					<img src="/icons/navbar/shin-chan-pfp.png" className="w-8 h-8 rounded-full" />
-				) : (
-					<img src="/icons/navbar/unsigned-user-profile.png" className="w-7 h-7 rounded-full" />
-				)}
-			</div>
->>>>>>> 93a2e6609c7419898473533b42c078f7d6561893
-			{NAVBAR_LINKS.map((link, index) => (
+			{/* {NAVBAR_LINKS.map((link, index) => (
 				<NavbarLink
 					key={index}
 					href={link.href}
@@ -67,7 +29,7 @@ const Navbar = () => {
 					onSelect={() => handlePageSelect(link.href)}
 					selectedPage={selectedPage}
 				/>
-			))}
+			))} */}
 		</div>
 	);
 };
