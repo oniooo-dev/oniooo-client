07302320dde3b01d723882916b472ed65207f62c
oniooo-dev <melody@oniooo.com>
2024-11-08 15:56:02 -0500

    rounded video frame

--- src/components/melody/messages/files/VideoFile.tsx
+++ src/components/melody/messages/files/VideoFile.tsx
@@ -7,7 +7,7 @@ interface VideoFileProps {
 const VideoFile: React.FC<VideoFileProps> = ({ uri }) => {
     return (
         <div className={`relative p-2 h-72 a-auto object-contain bg-white bg-opacity-15 rounded-2xl`}>
-            <video controls style={{ width: '100%' }}>
+            <video className="rounded-xl" controls style={{ width: '100%' }}>
                 <source src={uri} type="video/mp4" />
             </video>
         </div>
