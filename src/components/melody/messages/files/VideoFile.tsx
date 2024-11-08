c655a988db2515e3c8dab81f760a0fb1af6101b5
oniooo-dev <melody@oniooo.com>
2024-11-08 15:54:44 -0500

    video frame size

--- src/components/melody/messages/files/VideoFile.tsx
+++ src/components/melody/messages/files/VideoFile.tsx
@@ -6,7 +6,7 @@ interface VideoFileProps {
 
 const VideoFile: React.FC<VideoFileProps> = ({ uri }) => {
     return (
-        <div className={`relative w-96 p-2 h-80 object-contain bg-white bg-opacity-15 rounded-2xl`}>
+        <div className={`relative p-2 h-72 a-auto object-contain bg-white bg-opacity-15 rounded-2xl`}>
             <video controls style={{ width: '100%' }}>
                 <source src={uri} type="video/mp4" />
             </video>
