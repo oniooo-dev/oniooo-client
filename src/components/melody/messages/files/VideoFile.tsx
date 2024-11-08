24bd23e6323325accbab2703bd647e10886606f2
oniooo-dev <melody@oniooo.com>
2024-11-08 15:56:32 -0500

    video size fix

--- src/components/melody/messages/files/VideoFile.tsx
+++ src/components/melody/messages/files/VideoFile.tsx
@@ -6,7 +6,7 @@ interface VideoFileProps {
 
 const VideoFile: React.FC<VideoFileProps> = ({ uri }) => {
     return (
-        <div className={`relative p-2 h-72 a-auto object-contain bg-white bg-opacity-15 rounded-2xl`}>
+        <div className={`relative p-2 h-72 a-auto object-contain bg-white bg-opacity-15 rounded-2xl`} style={{ maxWidth: '100%' }}>
             <video className="rounded-xl" controls style={{ width: '100%' }}>
                 <source src={uri} type="video/mp4" />
             </video>
