e1911b21fb00b4faad8559eebf6e1cb383f710ec
oniooo-dev <melody@oniooo.com>
2024-11-08 15:59:12 -0500

    video frame fix

--- src/components/melody/messages/files/VideoFile.tsx
+++ src/components/melody/messages/files/VideoFile.tsx
@@ -6,8 +6,15 @@ interface VideoFileProps {
 
 const VideoFile: React.FC<VideoFileProps> = ({ uri }) => {
     return (
-        <div className={`relative p-2 h-72 a-auto object-contain bg-white bg-opacity-15 rounded-2xl`} style={{ maxWidth: '100%' }}>
-            <video className="rounded-xl" controls style={{ width: '100%' }}>
+        <div
+            className={`relative p-2 h-72 max-w-full bg-white bg-opacity-15 rounded-2xl overflow-hidden`}
+            style={{ maxWidth: '100%' }}
+        >
+            <video
+                className="rounded-xl w-full h-full object-contain"
+                controls
+                style={{ width: '100%', height: '100%' }}
+            >
                 <source src={uri} type="video/mp4" />
             </video>
         </div>
