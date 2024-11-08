0f13d859aac3f11fb2abb443f79200fc925f278c
oniooo-dev <melody@oniooo.com>
2024-11-08 15:53:11 -0500

    video frame fix

--- src/components/melody/messages/files/VideoFile.tsx
+++ src/components/melody/messages/files/VideoFile.tsx
@@ -6,7 +6,7 @@ interface VideoFileProps {
 
 const VideoFile: React.FC<VideoFileProps> = ({ uri }) => {
     return (
-        <div className="video-container" style={{ maxWidth: '100%' }}>
+        <div className={`relative w-96 p-2 h-80 object-contain bg-white bg-opacity-15 rounded-2xl`}>
             <video controls style={{ width: '100%' }}>
                 <source src={uri} type="video/mp4" />
             </video>
